8968276e67a124d1ec3278c30a6acf9bebf4b331
jayasriganesh <r0231jai@gmail.com>
2025-11-06 20:15:27 +0530

    add printFullName function

---
+++ assignments/functions/printFullName.js
@@ -0,0 +1,10 @@
+//syntax for creating a function is function fcn-name(parameter,parameter)
+function printFullName(firstName ,lastname){
+ return firstName+" "+lastname;
+}
+console.log(printFullName("JAYASRI","RAM"));
+//function printFullName is called and JAYASRI,RAM are passed as arguments.
+//returns JAYASRI RAM
+console.log(printFullName("JAYASRI","GANESH"));
+//returns JAYASRI GANESH
+//function printFullName is called and JAYASRI,GANESH are passed as arguments.
